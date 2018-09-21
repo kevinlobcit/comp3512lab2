c96dc6c2fac0c3b2807ba66725fee1b266d1534e
Kevin Lo <klo74@my.bcit.ca>
2018-09-21 14:54:28 -0700

    can read from Readings.txt

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <ctime>
 #include <random>
 #include <iomanip>
+#include <vector>
 
 void writeRandoms() {
     std::ofstream readings;
@@ -24,14 +25,46 @@ void writeRandoms() {
         randDouble = distribution(generator);
         readings << i << " " << std::fixed << std::setprecision(3) << randDouble << std::endl;
     }
+    readings.close();
 }
 
 int main()
 {
-    writeRandoms();
+    //writeRandoms();
 
+    int count = 0;
+    double value = 0;
 
+    double avg = 0;
+    double highest = 0;
+    double lowest = 0;
+    double median = 0;
 
+    std::vector <double> doubleVector;
+
+    std::ifstream inFile;
+    inFile.open("Readings.txt");
+
+    std::string line;
+    std::istringstream iss;
+    while(getline(inFile, line))
+    {
+
+        iss.clear();
+        iss.str(line);
+        std::cout << line << std::endl;
+
+        iss >> count;
+        //std::cout << count << " ";
+        iss >> value;
+        //std::cout << value << std::endl;
+        doubleVector.push_back(value);
+
+    }
+
+    std::cout << "now outputting doublevector values" << std::endl;
+
+    std::cout << "Done";
 
     return 0;
 }
