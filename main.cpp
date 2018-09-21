98fa645750b1981db33a39753d1ff8eb3aab33ed
Kevin Lo <klo74@my.bcit.ca>
2018-09-21 14:17:05 -0700

    Added the int and double randoms to print to Readings.txt

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include <iomanip>
+
 int main()
 {
+    std::ofstream readings;
+    readings.open("Readings.txt");
 
+    srand (time(NULL));
+    std::default_random_engine generator(time(0));
+    std::uniform_real_distribution<double>distribution(50.000, 90.000);
 
+    int count = (rand()%513)+512;
+    double randDouble = 0;
 
     std::cout<<"Lab2!"<<std::endl;
+    std::cout<< count;
+
+    for(int i = 0; i < count; i++) {
+        randDouble = distribution(generator);
+        readings << i << " " << std::fixed << std::setprecision(3) << randDouble << std::endl;
+    }
+
+
+
     return 0;
 }
